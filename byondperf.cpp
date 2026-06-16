b5a680f6ab186a3aa91c768ece4a5c605357eef7
antropod <antropod@gmail.com>
2026-06-17 02:07:58 +0600

    MemoryStats

--- byondperf.cpp
+++ byondperf.cpp
@@ -1,6 +1,56 @@
 #include <windows.h>
 #include <byondapi.h>
 
+struct DungServer;
+typedef char* DMString; // it is not real char*, there is some meta data (ptr-4 is refcnt, ptr-8 is ???) so don't free() it.
+
+#define LOAD_FUNCTION(dll, name) (name = (fp_##name)(GetProcAddress(dll, nm_##name)))
+
+typedef DMString* (__thiscall* fp_DungServer_GetServerMemUsage)(DungServer* this_, DMString* outvar);
+fp_DungServer_GetServerMemUsage DungServer_GetServerMemUsage;
+constexpr const char* nm_DungServer_GetServerMemUsage = "?GetServerMemUsage@DungServer@@QAE?AUDMString@@XZ";
+
+typedef void(__thiscall* fp_DMString_Destructor)(DMString* this_);
+fp_DMString_Destructor DMString_Destructor;
+constexpr const char* nm_DMString_Destructor = "??1DMString@@QAE@XZ";
+
+bool LoadFunctions()
+{
+    HMODULE byondcore = GetModuleHandleA("byondcore.dll");
+    if (!byondcore) {
+        return false;
+    }
+
+    if (!LOAD_FUNCTION(byondcore, DungServer_GetServerMemUsage)) {
+        return false;
+    }
+
+    if (!LOAD_FUNCTION(byondcore, DMString_Destructor)) {
+        return false;
+    }
+
+    return true;
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        if (!LoadFunctions()) {
+            Byond_CRASH("Failed to load byondcore.dll functions");
+        }
+        break;
+    case DLL_THREAD_ATTACH:
+        break;
+    case DLL_THREAD_DETACH:
+        break;
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
 extern "C" BYOND_EXPORT
 CByondValue PerfMark(u4c n, CByondValue v[])
 {
@@ -91,5 +141,29 @@ CByondValue PerfElapsed(u4c n, CByondValue v[])
     double elapsed = (pcEnd.QuadPart - pcStart.QuadPart) * scale / (double)pcFreq.QuadPart;
 
     ByondValue_SetNum(&ret, (float)elapsed);
+    return ret;
+}
+
+extern "C" BYOND_EXPORT
+CByondValue MemoryStats(u4c n, CByondValue v[])
+{
+    // This function gets memory usage of Byond Server as if you pressed
+    // Ctrl+M in dream daemon or typed "memory" in console dd
+    // 
+    // It calls internal "DungServer::GetServerMemUsage()" and returns a string
+    // This is not official or approved api, this is just a hack
+    // so this may leak memory, break, crash game. Use at your own risk.
+
+    CByondValue ret;
+    ByondValue_Clear(&ret);
+
+    if (n != 0) {
+        Byond_CRASH("Invalid number of arguments passed, expected 0");
+    }
+
+    DMString mem_usage_str;
+    DungServer_GetServerMemUsage(nullptr, &mem_usage_str);
+    ByondValue_SetStr(&ret, (char*)mem_usage_str);
+    DMString_Destructor(&mem_usage_str);
     return ret;
 }
--- byondperf.dm
+++ byondperf.dm
@@ -1,10 +1,13 @@
-var/_perf_mark = load_ext("byondperf", "byond:PerfMark")
-var/_perf_elapsed = load_ext("byondperf", "byond:PerfElapsed")
+#define DLL "byondperf"
 
-/proc/perf_mark()
-	return call_ext(_perf_mark)()
+var/_perf_mark = load_ext(DLL, "byond:PerfMark")
+var/_perf_elapsed = load_ext(DLL, "byond:PerfElapsed")
+var/_memory_stats = load_ext(DLL, "byond:MemoryStats")
+#define memory_stats call_ext(_memory_stats)
 
-//#define perf_mark(...) call_ext(_perf_mark)()
+#define perf_mark(...) call_ext(_perf_mark)()
 #define perf_seconds(start, end) call_ext(_perf_elapsed)(start, end, 1)
 #define perf_milliseconds(start, end) call_ext(_perf_elapsed)(start, end, 1000)
 #define perf_microseconds(start, end) call_ext(_perf_elapsed)(start, end, 1000000)
+
+#undef DLL
